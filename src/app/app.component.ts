4bcb7620162c72af38ffea984ea81bca58e3f76d
marcin.matula <marcin.matula@cie.men.gov.pl>
2017-03-30 13:48:01 +0200

    simple counter implemented on redux system fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,6 @@ import { Store } from '@ngrx/store';
 import { INCREMENT, DECREMENT, RESET } from './counter';
 import {Observable} from "rxjs/Observable";
 
-interface AppState {
-  counter: number;
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -16,7 +12,7 @@ export class AppComponent {
 
   counter:Observable<number>;
 
-  constructor(private store: Store<AppState>){
+  constructor(private store: Store<number>){
     this.counter = store.select('counter');
   }
 
